44b8d1954bd2a26ca93b6555022abd82fad7ef94
henryngai <Henryngai98@gmail.com>
2019-10-22 12:57:06 -0400

    initial commit

---
+++ src/main/java/com/SimLab/SimLab/SimLabApplication.java
@@ -0,0 +1,13 @@
+package com.SimLab.SimLab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimLabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SimLab/SimLab/SimLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SimLab.SimLab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
