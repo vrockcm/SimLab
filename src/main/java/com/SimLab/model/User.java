7ae1cda54bb16fca9e33bd565a5675781c723cc6
henryngai <Henryngai98@gmail.com>
2019-11-11 10:29:06 -0500

    Updated Login

--- src/main/java/com/SimLab/model/User.java
+++ src/main/java/com/SimLab/model/User.java
@@ -1,24 +1,39 @@
 package com.SimLab.model;
+import lombok.Data;
+import org.hibernate.validator.constraints.Length;
 
-import com.SimLab.model.Role;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import java.util.Set;
 
+@Data
 @Entity
+@Table(name = "user")
 public class User {
-    @Id
-    @Column(name = "id")
-    private int id;
 
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "user_id")
+    private int id;
     @Column(name = "email")
+    @Email(message = "*Please provide a valid Email")
+    @NotEmpty(message = "*Please provide an email")
     private String email;
     @Column(name = "password")
+    @Length(min = 5, message = "*Your password must have at least 5 characters")
+    @NotEmpty(message = "*Please provide your password")
     private String password;
-    @Column(name = "role")
-    private Role role;
-
-
+    @Column(name = "first_name")
+    @NotEmpty(message = "*Please provide your name")
+    private String name;
+    @Column(name = "last_name")
+    @NotEmpty(message = "*Please provide your last name")
+    private String lastName;
+    @Column(name = "active")
+    private int active;
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles;
 
 }
