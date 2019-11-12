ad0465b7a065ac488737989144d0900fe4a4a69c
henryngai <Henryngai98@gmail.com>
2019-11-12 18:45:04 -0500

    Updated lab query

--- src/main/java/com/SimLab/model/dao/Repository/CourseLabAssociationRepository.java
+++ src/main/java/com/SimLab/model/dao/Repository/CourseLabAssociationRepository.java
@@ -1,9 +1,16 @@
 package com.SimLab.model.dao.Repository;
 
 import com.SimLab.model.dao.CourseLabAssociation;
+import com.SimLab.model.dao.Lab;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface CourseLabAssociationRepository extends CrudRepository <CourseLabAssociation, Long> {
+    @Query("SELECT y FROM CourseLabAssociation x, Lab y WHERE  x.labId = y.labId  AND x.courseId = :courseId")
+    public List<Lab> loadAssociatedLabs(@Param("courseId") int courseId);
 }
