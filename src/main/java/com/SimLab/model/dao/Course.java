e5ab55c25ac7a8d9ffce0e1e8808283ef81c8290
henryngai <Henryngai98@gmail.com>
2019-11-12 15:38:55 -0500

    Fixed query as well as loading courses correctly.

--- src/main/java/com/SimLab/model/dao/Course.java
+++ src/main/java/com/SimLab/model/dao/Course.java
@@ -4,6 +4,30 @@ import javax.persistence.*;
 
 @Entity
 public class Course {
+    public int getCourseId() {
+        return courseId;
+    }
+
+    public void setCourseId(int courseId) {
+        this.courseId = courseId;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public String getCourseDesc() {
+        return courseDesc;
+    }
+
+    public void setCourseDesc(String courseDesc) {
+        this.courseDesc = courseDesc;
+    }
+
     @Id
     @GeneratedValue (strategy = GenerationType.AUTO)
     @Column(name = "Id")
