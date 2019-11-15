09ec76981a26dc05f03dcbdf6a56a1366a4e9291
Basim <basim.chowdhury@stonybrook.edu>
2019-11-15 03:35:33 -0500

    ajax call for loading a prev course onto the edit form

---
+++ src/main/java/com/SimLab/model/CourseInfo.java
@@ -0,0 +1,20 @@
+package com.SimLab.model;
+
+import com.SimLab.model.dao.Course;
+import com.SimLab.model.dao.User;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class CourseInfo {
+
+    private String courseName;
+
+    private String courseDesc;
+
+    private List<User> students;
+    private List<User> instructors;
+    private List<User> allStudents;
+    private List<User> allInstructors;
+}
