38cd1059183b9281fe09bae75e99b40168b28fb7
Yousef Khan <yousef.khan@stonybrook.edu>
2019-12-10 23:50:22 -0500

    measureDrawout verify

--- src/main/java/com/SimLab/model/workbench/InstructionObjects/MeasureDrawOut_Backend.java
+++ src/main/java/com/SimLab/model/workbench/InstructionObjects/MeasureDrawOut_Backend.java
@@ -1,13 +1,42 @@
 package com.SimLab.model.workbench.InstructionObjects;
 
 import com.SimLab.model.dao.Tool;
+import com.SimLab.model.workbench.MaterialObjects.BkendContainer;
+import com.SimLab.model.workbench.MaterialObjects.BkendSolution;
+import com.SimLab.model.workbench.MaterialObjects.BkendTool;
+
+import java.util.List;
 
 public class MeasureDrawOut_Backend implements Instruction {
-    public String material;
-    public Tool measuringTool;
+//    public String material;
+    public BkendTool measuringTool;
+    public BkendSolution mysol;
+    public BkendContainer mycont;
+    public List<BkendContainer> contlist;
+    public List<BkendSolution> solslist;
+    public int targetvol;
+
+    public MeasureDrawOut_Backend(BkendTool tool, BkendContainer container, BkendSolution solution, int targetVolume){
+        this.mysol = solution;
+        this.measuringTool = tool;
+        this.targetvol = targetVolume;
+        this.mycont = container;
+    }
 
     @Override
     public boolean verify() {
+        for(BkendContainer c: contlist){
+            if(c.getName().equals(mycont.getName())){
+                for(BkendSolution s: c.getSolutions()){
+                    if((s.getSolution().getName().equals(mysol.getSolution().getName()))
+                    && mysol.getSolution().getVolume() == targetvol){
+                        return true;
+                    }
+                }
+            }
+
+        }
+
         return false;
     }
 }
